6db06b1947b2bf74659422c98e5c214af0cb50bd
Alejandro Aguilera garcia <aaguiler@c3r7s5.42malaga.com>
2022-08-23 18:43:39 +0200

    Add condition n arg

--- src/parse_command.c
+++ src/parse_command.c
@@ -6,7 +6,7 @@
 /*   By: aaguiler <aaguiler@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 17:54:51 by aaguiler          #+#    #+#             */
-/*   Updated: 2022/08/23 18:16:11 by aaguiler         ###   ########.fr       */
+/*   Updated: 2022/08/23 18:37:37 by aaguiler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,27 @@
 
 int	ft_get_n_args(char *cmd)
 {
+	int	quotes;
 	int	n_args;
 	int	i;
 
+	quotes = 0;
 	n_args = 1;
 	i = 0;
-	while(cms[i])
+	while(cmd[i])
 	{
+		if (cmd[i] == '\"' && quotes == 0)
+			quotes = 2;
+		else if (cmd[i] == '\"' && quotes == 2)
+			quotes = 0;
+		else if (cmd[i] == '\'' && quotes == 0)
+			quotes = 1;
+		else if (cmd[i] == '\'' && quotes == 1)
+			quotes = 0;
+		else if (cmd[i] == ' ' && !quotes)
+			n_args++;
+		while (cmd[i] && cmd[i] == ' ')
+			i++;
 		i++;
 	}
 	return (n_args);
@@ -33,6 +47,7 @@ t_command	*ft_parse_command(char *cmd)
 	command = malloc(sizeof(t_command*) * 1);
 	if (!command)
 		return (0);
+	cmd = ft_strtrim(cmd, " ");
 	command->n_args = ft_get_n_args(cmd);
 	return (command);
 }
