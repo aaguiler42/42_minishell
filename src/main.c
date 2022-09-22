0a38ec60439fbbb302defc31e4e0d82304da36a6
Alejandro Aguilera garcia <aaguiler@c1r8s1.42malaga.com>
2022-09-22 17:43:17 +0200

    Refactor code

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aaguiler <aaguiler@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 15:53:20 by aaguiler          #+#    #+#             */
-/*   Updated: 2022/09/22 17:28:56 by aaguiler         ###   ########.fr       */
+/*   Updated: 2022/09/22 17:42:33 by aaguiler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,11 @@ char	*ft_get_line(void)
 	line = ft_strtrim_spaces(line);
 	if (!line)
 		return (NULL);
+	if (!ft_check_errors(line))
+	{
+		free(line);
+		return (NULL);	
+	}
 	return (line);
 }
 
@@ -51,15 +56,10 @@ int	main(int argc, char **argv, char **env)
 		line = ft_get_line();
 		if (!line)
 			break ;
-		if (!ft_check_errors(line))
-			free(line);
-		else
-		{
-			commands = ft_get_commands(line);
-			if (!commands && line[0])
-				break ;
-			// Aquí se ejecutan los comandos
-		}
+		commands = ft_get_commands(line);
+		if (!commands && line[0])
+			break ;
+		// Aquí se ejecutan los comandos
 	}
 	ft_lstclear(&g_env_vars, free);
 	clear_history();
